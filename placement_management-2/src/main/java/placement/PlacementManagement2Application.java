e792dcada490aef760ef22dff44ab0fa5f0e99c2
pavi5 <pavi5@LAPTOP-B44HQDM0>
2023-02-19 18:26:40 +0530

    first commit

---
+++ placement_management-2/src/main/java/placement/PlacementManagement2Application.java
@@ -0,0 +1,13 @@
+package placement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementManagement2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementManagement2Application.class, args);
+	}
+
+}
